9bdec0910d726d7d1e56d7ab23644b7113753727
austinhang <91672423+akirahang@users.noreply.github.com>
2024-06-13 10:52:10 +0800

    Update xrayr-install.sh

--- xrayr-install.sh
+++ xrayr-install.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+
+# 进入 /root/ 目录
+cd /root/
+
+# 克隆 GitHub 仓库
+git clone https://github.com/XrayR-project/XrayR-release
+
+# 进入克隆的目录
+cd XrayR-release
+
+# 使用 Docker Compose 启动容器并以后台模式运行
+docker-compose up -d
