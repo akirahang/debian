8349dbd0704729e42fa20591685b0f06a714cf12
austinhang <91672423+akirahang@users.noreply.github.com>
2024-11-15 08:47:24 +0800

    Update nps.sh

--- nps.sh
+++ nps.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# 克隆nps项目
-cd /root/
-git clone https://github.com/ehang-io/nps.git
+# 设置 NPS 项目路径
+NPS_DIR="/root/nps"
+
+# 检查 Docker Compose 是否已安装
+command -v docker >/dev/null 2>&1 || { echo "请先安装 Docker"; exit 1; }
+
+# 克隆 NPS 项目
+mkdir -p "$NPS_DIR"
+cd "$NPS_DIR"
+git clone https://github.com/ehang-io/nps.git . || { echo "克隆 NPS 代码失败"; exit 1; }
 
 # 交互式获取配置参数
 read -p "请输入http_proxy_port的值: " http_proxy_port
@@ -13,12 +20,14 @@ read -p "请输入web_ip的值: " web_ip
 read -p "请输入auth_crypt_key的值: " auth_crypt_key
 
 # 修改配置文件
-sed -i "s/^http_proxy_port=.*$/http_proxy_port=${http_proxy_port}/" /root/nps/conf/nps.conf
-sed -i "s/^https_proxy_port=.*$/https_proxy_port=${https_proxy_port}/" /root/nps/conf/nps.conf
-sed -i "s/^web_username=.*$/web_username=${web_username}/" /root/nps/conf/nps.conf
-sed -i "s/^web_password=.*$/web_password=${web_password}/" /root/nps/conf/nps.conf
-sed -i "s/^web_ip=.*$/web_ip=${web_ip}/" /root/nps/conf/nps.conf
-sed -i "s/^auth_crypt_key=.*$/auth_crypt_key=${auth_crypt_key}/" /root/nps/conf/nps.conf
+sed -i "s/^http_proxy_port=.*$/http_proxy_port=${http_proxy_port}/" conf/nps.conf
+sed -i "s/^https_proxy_port=.*$/https_proxy_port=${https_proxy_port}/" conf/nps.conf
+sed -i "s/^web_username=.*$/web_username=${web_username}/" conf/nps.conf
+sed -i "s/^web_password=.*$/web_password=${web_password}/" conf/nps.conf
+sed -i "s/^web_ip=.*$/web_ip=${web_ip}/" conf/nps.conf
+sed -i "s/^auth_crypt_key=.*$/auth_crypt_key=${auth_crypt_key}/" conf/nps.conf
 
-# 运行Docker容器
-docker run -d --name nps --net=host -v /root/nps/conf:/conf --restart=always ffdfgdfg/nps
+# 运行 Docker 容器
+docker run -d --name nps --net=host -v "$NPS_DIR/conf":/conf --restart=always ffdfgdfg/nps || { echo "启动 NPS 容器失败"; exit 1; }
+
+echo "NPS 容器已启动，请访问 http://你的IP:8080 进行管理"
